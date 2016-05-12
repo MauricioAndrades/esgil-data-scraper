3c32da14d270e9b815e3e9465cde7f19ece331dc
Mauricio Andrades <mod.and.op@gmail.com>
2016-05-11 19:58:24 -0700

    added sites array

---
+++ app.js
@@ -0,0 +1,14 @@
+var Nightmare = require('./');
+var nightmare = Nightmare({
+	show: true
+});
+
+var sites = [
+	"http://www.californiabids.com/",
+	"http://www.nevadabids.com/",
+	"http://www.montanabids.com/",
+	"http://www.oregonbids.com",
+	"http://www.washingtonbids.com",
+	"http://www.utahbids.net",
+	"http://www.idahobids.com"
+];
