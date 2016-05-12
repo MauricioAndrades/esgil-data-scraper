1b62b52c0a40bf4820abcffde1c9692b924a6301
Mauricio Andrades <mod.and.op@gmail.com>
2016-05-11 20:06:08 -0700

    added keywords

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var Nightmare = require('./');
+var Nightmare = require('nightmare');
 var nightmare = Nightmare({
 	show: true
 });
@@ -12,3 +12,25 @@ var sites = [
 	"http://www.utahbids.net",
 	"http://www.idahobids.com"
 ];
+
+var keywords = [
+	"building department",
+	"building dept",
+	"building plan",
+	"building review",
+	"code compliance",
+	"code review",
+	"plan check",
+	"plan review"
+];
+
+nightmare
+	.goto(sites[0])
+	.wait('#main')
+	.end()
+	.then(function(result) {
+		console.log(result)
+	})
+	.catch(function(e) {
+		console.error(e)
+	})
