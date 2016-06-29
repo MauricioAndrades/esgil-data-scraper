1ee33c57802f2866344d94c154b2a623ceb40eec
Mauricio Andrades <mod.and.op@gmail.com>
2016-06-28 19:41:35 -0700

    no matches

--- app.js
+++ app.js
@@ -15,143 +15,158 @@ var Readable = require('stream').Readable;
 var $;
 var masteraccum = [];
 var cheerioOptions = {
-  withDomLvl1: true,
-  normalizeWhitespace: false,
-  xmlMode: false,
-  decodeEntities: true
+    withDomLvl1: true,
+    normalizeWhitespace: false,
+    xmlMode: false,
+    decodeEntities: true
 };
+var global_bin = [];
 
-var testfind = function(target) {
-  var searchpattern = /.*building dept.*|.*building deptartment.*|.*plan review.*|.*building plan.*|.*building code.*|.*code compliance.*|.*code review.*|.*plan check.*/ig
-  var oRegex = new RegExp(searchpattern);
-  for(var key in target) {
-    var str = target[key];
-  }
-  var found = String(target).search(oRegex);
-  if (str.search(re) != -1) {
-    return true
-  } else
-  return false;
-}
+/////////////////////////// traverse inspection ///////////////////////////
 
-/////////////////////////
-// traverse inspection //
-/////////////////////////
 function scrubobj(obj, callback) {
-  var scrubbed =
-    traverse(obj).map(function(x) {
-      if (this.circular) this.remove();
-    });
-  callback(scrubbed);
+    var scrubbed =
+        traverse(obj).map(function(x) {
+            if (this.circular) this.remove();
+        });
+    callback(scrubbed);
 }
 
 function walkobject(obj) {
-  let s = '';
-  let walked =
-    traverse(obj).forEach(function to_s(node) {
-      if (Array.isArray(node)) {
-        this.before(function() {
-          s += '['
+    let s = '';
+    let walked =
+        traverse(obj).forEach(function to_s(node) {
+            if (Array.isArray(node)) {
+                this.before(function() {
+                    s += '['
+                });
+                this.post(function(child) {
+                    if (!child.isLast) s += ',';
+                });
+                this.after(function() {
+                    s += ']'
+                });
+            } else if (typeof node == 'object') {
+                this.before(function() {
+                    s += '{'
+                });
+                this.pre(function(x, key) {
+                    to_s(key);
+                    s += ':';
+                });
+                this.post(function(child) {
+                    if (!child.isLast) s += ',';
+                });
+                this.after(function() {
+                    s += '}'
+                });
+            } else if (typeof node == 'string') {
+                s += '"' + node.toString().replace(/"/g, '\\"') + '"';
+            } else if (typeof node == 'function') {
+                s += 'null';
+            } else {
+                s += node.toString();
+                s += '\n'
+            }
         });
-        this.post(function(child) {
-          if (!child.isLast) s += ',';
-        });
-        this.after(function() {
-          s += ']'
-        });
-      } else if (typeof node == 'object') {
-        this.before(function() {
-          s += '{'
-        });
-        this.pre(function(x, key) {
-          to_s(key);
-          s += ':';
-        });
-        this.post(function(child) {
-          if (!child.isLast) s += ',';
-        });
-        this.after(function() {
-          s += '}'
-        });
-      } else if (typeof node == 'string') {
-        s += '"' + node.toString().replace(/"/g, '\\"') + '"';
-      } else if (typeof node == 'function') {
-        s += 'null';
-      } else {
-        s += node.toString();
-        s += '\n'
-      }
-    });
-  console.log(walked);
+    console.log(walked);
 }
 
-////////////////////
-// start promises //
-////////////////////
+////////////////////// start promises //////////////////////
 
 /** push promised requests into an array to resolve */
 for (var i = 0; i < urls.length; i++) {
-  promises.push(request.getAsync(urls[i]))
+    promises.push(request.getAsync(urls[i]))
 }
 
 // Promise.all(Iterable<any>|Promise<Iterable<any>> input) -> Promise
 Promise.all(promises).then(function(data) {
-  return writeFile('./promise.json', data, 'utf8')
+    return writeFile('./promise.json', data, 'utf8')
 }).then(function() {
-  return readFile('./promise-results.json', 'utf8')
+    return readFile('./promise-results.json', 'utf8')
 }).then(function(contents) {
-  debugger;
 
-  var parsed = JSON.parse(contents);
-  // contents arr of objs. [obj, obj, obj, obj, obj] parsed[0].body
-  var arr = [];
-  var container = {};
-
-  for (var i = 0; i <= parsed.length; i++) {
-    var $ = cheerio.load(parsed[i]["body"]);
-    // console.log($('tbody').contents().html())
-
-    $('tbody').children().contents().find('td>a').addBack().each(function() {
-      arr.push($(this));
-
-    });
-    var bin = {};
-    var formatted = [];
+    var parsed = JSON.parse(contents);
+    
+    /** @type {Object} local place to create objs before pushing them */
+    var _bin = {};
+    
+    /** @type {Number} local count to assemble objects */
     var count = 0;
-    for (var i = 0; i < arr.length; i++) {
-      count += 1;
-      bin[count] = $(arr[i]).text();
-      if ($(arr)[i].attr('href') !== undefined) {
-        bin[count] = ($(arr)[i].attr('href'));
-      }
-      if (bin[count] === " ") {
-        formatted.push(bin);
-        bin = {};
-        count = 0;
-      }
-    }
-    debugger;
-    for(var i = 0; i < formatted.length; i++) {
-      for (var key in formatted[i]) {
-        if(testfind(formatted[key])){
-          console.log(found);
-          console.log(formatted[key])
+
+    /** the results from cheerio parsing */
+    var arr = [];
+    
+    /**
+     * grep through keys, find matches
+     * @param  {[type]} literal_string [description]
+     * @param  {[type]} target_array   [description]
+     * @return {[type]}                [description]
+     */
+    function testfind(target) {
+
+        var oRegex = new RegExp(/.*building.*|.*building dept.*|.*building department.*|.*plan review.*|.*building plan.*|.*building code.*|.*code compliance.*|.*code review.*|.*plan check.*/mig);
+
+        var found = String(target[i]).search(oRegex);
+
+        if (found > -1) {
+            // match_arr.push(target[i]);
+            return true;
         }
-      }
+
+        return false;
     }
 
-    /////////////////////////////
-    // keywords match section  //
-    /////////////////////////////
+    /////////////////////////// cheerio load area  //////////////////////////
 
+    /** for each obj in the full collection read from file */
+    for (var j = 0; j < parsed.length; j++) {
 
+        $ = cheerio.load(parsed[j]["body"]);
 
-  }
+        /**
+         * cheerio parse table body
+         */
+        $('tbody').children()
+            .contents()
+            .find('td>a')
+            .addBack().each(function() {
+                arr.push($(this));
+            });
+
+        ///////////////////// extract data from arr of dom elements /////////////////////
+debugger;
+        /** loop through array of cheerio objs and pull out the properties we want from td */
+        for (var x = 0; x < arr.length; x++) {
+            count += 1;
+            _bin[count] = $(arr[x]).text();
+            if ($(arr)[x].attr('href') !== undefined) {
+                _bin[count] = ($(arr)[x].attr('href'));
+            }
+
+            /** reset on empty object key === separator line */
+            if (_bin[count] === " ") {
+                global_bin.push(_bin);
+                _bin = {};
+                count = 0;
+            }
+        }
+
+        /** check the objects for matched keys */
+        for (var k = 0; k < global_bin.length; k++) {
+            for (var key in global_bin[k]) {
+                //////////////////////////////////// insert test find logic here
+                if (testfind(global_bin[key]) === true) {
+                    console.log(global_bin[key]);
+                }
+            }
+        }
+    }
 }).catch(function(err) {
-  if (err) console.log(err);
-  require('node-clean-exit')();
+    if (err) console.log(err);
 });
 
+require('node-clean-exit')();
 // document.querySelector('tbody').innerText
 // non breaking space '\u00a0'
 // Basically jQuery for node.js
