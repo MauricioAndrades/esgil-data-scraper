24b08dca9173a2695a0b8cfe214c719b8b3fca7d
Mauricio Andrades <mod.and.op@gmail.com>
2016-06-29 14:06:46 -0700

    removed untracked

--- 5.js
+++
@@ -1,9 +0,0 @@
-var cheerio = require('cheerio');
-var fs = require('fs');
-var input = fs.readFileSync('./html.html', 'utf8');
-var $ = cheerio.load(input);
-
-
-
-console.log('---');
-console.log($('li.item-a').closest().prop('class'))
